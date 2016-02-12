413996e1ce449560c8fc5db566107c7770246109
Marcin Wrochniak <wrochniak@gmail.com>
2016-02-12 23:30:25 +0100

    Some generator code
    Real tests will need to be added, these are just copied from Typer to
    see if it works at all.

--- generator.go
+++ generator.go
@@ -1,6 +1,10 @@
 package have
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
 
 // CodeChunk can either be a string of smaller CodeChunks
 // or its value can be a string.
@@ -32,8 +36,84 @@ func (cc *CodeChunk) ReadAll() string {
 	return buf.String()
 }
 
+func (cc *CodeChunk) AddString(s string) {
+	cc.chunks = append(cc.chunks, cc.NewStrChunk(s))
+}
+
+func (cc *CodeChunk) AddChunks(chunks ...*CodeChunk) {
+	cc.chunks = append(cc.chunks, chunks...)
+}
+
+func (cc *CodeChunk) NewStrChunk(s string) *CodeChunk {
+	return &CodeChunk{code: s, parent: cc}
+}
+
+// Format with fmt.Sprintf, but one addition: "%C" can be use to add Generables.
+func (cc *CodeChunk) AddChprintf(format string, a ...interface{}) {
+	var nonGenerables []interface{}
+
+	parts := strings.Split(format, "%C")
+
+	i := 0
+	for _, arg := range a {
+		switch v := arg.(type) {
+		case Generable:
+			//generables = append(generables, v)
+			cc.AddString(fmt.Sprintf(parts[i], nonGenerables...))
+			nonGenerables = nil
+			i++
+
+			v.Generate(cc)
+		default:
+			nonGenerables = append(nonGenerables, v)
+		}
+	}
+
+	// Leftovers
+	cc.AddString(fmt.Sprintf(parts[i], nonGenerables...))
+}
+
 type Generable interface {
 	Generate(current *CodeChunk)
+}
+
+func (id *Ident) Generate(current *CodeChunk) {
+	current.AddString(id.name)
+}
+
+func (lit *BasicLit) Generate(current *CodeChunk) {
+	val := ""
+	switch lit.token.Type {
+	case TOKEN_NUM, TOKEN_TRUE, TOKEN_FALSE:
+		val = lit.token.Value.(string)
+	case TOKEN_STR:
+		val = fmt.Sprintf("\"%s\"", lit.token.Value.(string))
+	default:
+		panic("impossible")
+	}
+
+	// Don't generate type conversions if not necessary.
+	current.AddString(fmt.Sprintf("(%s)(%s)", lit.typ, val))
+}
+
+func (op *UnaryOp) Generate(current *CodeChunk) {
+	// TODO: Put the right operator in
+	current.AddChprintf("(%s(%C))", "+", op.Right.(Generable))
+}
+
+func (op *BinaryOp) Generate(current *CodeChunk) {
+	// TODO: Put the right operator in
+	current.AddChprintf("((%C)%s(%C))", op.Left.(Generable), "+", op.Right.(Generable))
+}
+
+func (vd *VarDecl) Generate(current *CodeChunk) {
+	current.AddChprintf("var %s %s = (%C)\n", vd.name, vd.Type, vd.Init.(Generable))
+}
+
+func (vs *VarStmt) Generate(current *CodeChunk) {
+	for _, vd := range vs.Vars {
+		vd.Generate(current)
+	}
 }
 
 // TODO: Now just write Generables for all statements/expressions and we're done...
--- generator_test.go
+++ generator_test.go
@@ -1,1 +1,68 @@
 package have
+
+import (
+	"fmt"
+	"testing"
+)
+
+type generateTestCase struct {
+	code       string
+	shouldPass bool
+	typ        string
+}
+
+func testVarGenerate(t *testing.T, cases []generateTestCase) {
+	for i, c := range cases {
+		parser := NewParser(NewLexer([]rune(c.code)))
+		result, err := parser.Parse()
+		if err != nil {
+			t.Fail()
+			fmt.Printf("FAIL: Failed parsing: %s\n", err)
+		}
+
+		var varStmt *VarStmt = nil
+		var ok = false
+
+		for _, stmt := range result {
+			varStmt, ok = stmt.(*VarStmt)
+			if ok {
+				err = varStmt.NegotiateTypes()
+				if err != nil {
+					break
+				}
+			}
+
+			cc := &CodeChunk{}
+			varStmt.Generate(cc)
+			fmt.Printf("ZZZ \n\n--------\n%s\n--------\n\n", cc.ReadAll())
+		}
+
+		if (err == nil) != c.shouldPass {
+			t.Fail()
+			fmt.Printf("FAIL: Case %d: Bad code accepted or good code parsed with an error for '%s'\nError: %s\n",
+				i, c.code, err)
+			return
+		}
+
+		if c.shouldPass {
+			firstVar := varStmt.Vars[0]
+			if firstVar.Type.String() != c.typ || !IsAssignable(firstVar.Init.(TypedExpr).Type(), firstVar.Type) {
+				t.Fail()
+				fmt.Printf("FAIL: Case %d: Bad type: %s, %s, %s\n", i, c.typ, firstVar.Type.String(),
+					firstVar.Init.(TypedExpr).Type().String())
+			}
+		}
+	}
+}
+
+func TestGenerate(t *testing.T) {
+	var cases = []generateTestCase{
+		{`var b int = 2		
+var a int = b+2`,
+			true,
+			"int",
+		},
+	}
+
+	testVarGenerate(t, cases)
+}
