689be3478533caca91a622622d4be77748bc16aa
Marcin Wrochniak <wrochniak@gmail.com>
2016-05-10 23:08:44 +0200

    Added ident_stack.go

--- parser.go
+++ parser.go
@@ -21,55 +21,6 @@ type Parser struct {
 	prevLbl *LabelStmt // Just declared labal is stored here temporarily
 }
 
-// Stack of scopes available to the piece of code that is currently
-// being parsed. It is a living stack, scopes are pushed to and popped
-// from it as new blocks of code start and end.
-// It is used for initial bonding of names and objects (packages,
-// variables, types), which later helps the type checker.
-type IdentStack []map[string]Object
-
-func (is *IdentStack) pushScope() {
-	*is = append(*is, map[string]Object{})
-}
-
-func (is *IdentStack) popScope() {
-	*is = (*is)[:len(*is)-1]
-}
-
-func (is *IdentStack) empty() bool {
-	return len(*is) == 0
-}
-
-func (is *IdentStack) addObject(v Object) {
-	(*is)[len(*is)-1][v.Name()] = v
-}
-
-// Returns nil when not found
-func (is *IdentStack) findObject(name string) Object {
-	if decl, ok := GetBuiltinType(name); ok {
-		return decl
-	}
-	for i := len(*is) - 1; i >= 0; i-- {
-		if v, ok := (*is)[i][name]; ok {
-			return v
-		}
-	}
-	return nil
-}
-
-// Returns nil when not found
-func (is *IdentStack) findTypeDecl(name string) *TypeDecl {
-	if decl, ok := GetBuiltinType(name); ok {
-		return decl
-	}
-	for i := len(*is) - 1; i >= 0; i-- {
-		if v, ok := (*is)[i][name]; ok && v.ObjectType() == OBJECT_TYPE {
-			return v.(*TypeDecl)
-		}
-	}
-	return nil
-}
-
 func (p *Parser) nextToken() *Token {
 	if len(p.tokensBuf) > 0 {
 		result := p.tokensBuf[0]
