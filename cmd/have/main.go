ecb29242911f539081b432801cacf90a28d351a9
Marcin Wrochniak <wrochniak@gmail.com>
2016-08-31 23:55:14 +0200

    Added 'have run' command

--- cmd/have/main.go
+++ cmd/have/main.go
@@ -6,8 +6,10 @@ import (
 	"have/have"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path"
 	"strings"
+	"syscall"
 )
 
 // Implements PkgLocator
@@ -45,6 +47,21 @@ func (gpl *FilesystemPkgLocator) Locate(relativePath string) ([]*have.File, erro
 	return files, nil
 }
 
+func paths() (gopath, srcpath string) {
+	gopath = os.Getenv("GOPATH")
+	if gopath == "" {
+		fmt.Fprintf(os.Stderr, "GOPATH not set")
+		os.Exit(1)
+	}
+
+	srcpath = os.Getenv("HAVESRCPATH")
+	if srcpath == "" {
+		srcpath = path.Join(gopath, "src")
+	}
+
+	return
+}
+
 func trans(args []string) {
 	var pkgs, files []string
 	for _, arg := range args {
@@ -55,16 +72,7 @@ func trans(args []string) {
 		}
 	}
 
-	var gopath = os.Getenv("GOPATH")
-	if gopath == "" {
-		fmt.Fprintf(os.Stderr, "GOPATH not set")
-		os.Exit(1)
-	}
-
-	var srcpath = os.Getenv("HAVESRCPATH")
-	if srcpath == "" {
-		srcpath = path.Join(gopath, "src")
-	}
+	var gopath, srcpath = paths()
 
 	var locator = NewFilesystemPkgLocator(srcpath)
 
@@ -107,6 +115,106 @@ func trans(args []string) {
 	}
 }
 
+// Implements PkgLocator
+type RunLocator struct {
+	fpl          *FilesystemPkgLocator
+	mainPkgFiles []*have.File
+}
+
+func NewRunLocator(fpl *FilesystemPkgLocator, mainFilenames []string) (*RunLocator, error) {
+	var files []*have.File
+	for _, f := range mainFilenames {
+		code, err := ioutil.ReadFile(f)
+		if err != nil {
+			return nil, fmt.Errorf("Error reading %s: %s", f, err)
+		}
+
+		files = append(files, have.NewFile(f, string(code)))
+	}
+	return &RunLocator{
+		fpl:          fpl,
+		mainPkgFiles: files,
+	}, nil
+}
+
+func (rl *RunLocator) Locate(relativePath string) ([]*have.File, error) {
+	if relativePath == "main" {
+		return rl.mainPkgFiles, nil
+	}
+	return rl.fpl.Locate(relativePath)
+}
+
+func run(args []string) {
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, "No source files specified\n")
+		os.Exit(1)
+	}
+
+	for _, arg := range args {
+		if !strings.HasSuffix(arg, ".hav") {
+			fmt.Fprintf(os.Stderr, "Not a source file: %s", arg)
+		}
+	}
+
+	var _, srcpath = paths()
+
+	var locator, err = NewRunLocator(NewFilesystemPkgLocator(srcpath), args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating temporary dir: %s", err)
+		os.Exit(1)
+	}
+
+	manager := have.NewPkgManager(locator)
+
+	pkg, errs := manager.Load("main")
+
+	for _, err := range errs {
+		if compErr, ok := err.(*have.CompileError); ok {
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n", compErr.PrettyString(manager.Fset))
+		} else {
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+		}
+	}
+
+	if len(errs) > 0 {
+		os.Exit(1)
+	}
+
+	tmpDir, err := ioutil.TempDir("", "hav")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating temporary dir: %s", err)
+		os.Exit(1)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	var goFiles []string
+
+	for _, f := range pkg.Files {
+		if f.Name == have.BuiltinsFileName {
+			continue
+		}
+		var output = f.GenerateCode()
+
+		outputPath := path.Join(tmpDir, f.Name+".go")
+		ioutil.WriteFile(outputPath, []byte(output), 0600)
+		goFiles = append(goFiles, outputPath)
+	}
+
+	binary, err := exec.LookPath("go")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't find go binary: %s", err)
+		os.Exit(1)
+	}
+
+	env := os.Environ()
+
+	err = syscall.Exec(binary, append([]string{"go", "run"}, goFiles...), env)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error execing go binary: %s", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	flag.Parse()
 
@@ -120,6 +228,8 @@ func main() {
 	switch args[0] {
 	case "trans":
 		trans(args[1:])
+	case "run":
+		run(args[1:])
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", args[0])
 	}
