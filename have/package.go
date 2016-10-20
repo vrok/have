c25cfafa766d7d172f53044f68beabc4110ee1e1
Marcin Wrochniak <wrochniak@gmail.com>
2016-10-20 22:27:18 +0200

    Indents to braces: more tests, some fixes

--- have/package.go
+++ have/package.go
@@ -39,20 +39,20 @@ const BuiltinsFileName = "_builtin.hav"
 
 func builtinsFile(pkgName string) *File {
 	code := "package " + pkgName + `
-func print(s interface: pass) bool: return false
-func read() string: pass
-func len[T](c T) int: __compiler_macro("len(%a0)")
-func new[T]() *T: __compiler_macro("new(%t0)")
-func make[T](size int) T: __compiler_macro("make(%t0, %a0)")
+func print(s interface{}) bool { return false }
+func read() string { pass }
+func len[T](c T) int { __compiler_macro("len(%a0)") }
+func new[T]() *T { __compiler_macro("new(%t0)") }
+func make[T](size int) T { __compiler_macro("make(%t0, %a0)") }
 # TODO: Update append with varargs when varargs work
-func append[T](slice []T, elem T) []T: __compiler_macro("append(%a0, %a1)")
+func append[T](slice []T, elem T) []T { __compiler_macro("append(%a0, %a1)") }
 # TODO: Refactor cap() to be a generic with 'when' stmt after pattern
 # matching is added to 'when'
-func cap(v interface: pass) int: pass
-func copy[T](dst, src []T) int: __compiler_macro("copy(%a0, %a1)")
-func delete[T, K](m map[T]K, key T): __compiler_macro("delete(%a0, %a1)")
-func panic(v interface: pass): pass
-func close[T](c chan<- T): pass`
+func cap(v interface{}) int { pass }
+func copy[T](dst, src []T) int { __compiler_macro("copy(%a0, %a1)") }
+func delete[T, K](m map[T]K, key T) { __compiler_macro("delete(%a0, %a1)") }
+func panic(v interface{}) { pass }
+func close[T](c chan<- T) { pass }`
 	return &File{
 		Name: BuiltinsFileName,
 		Code: code,
